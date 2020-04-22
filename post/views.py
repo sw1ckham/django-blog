de79e7bac64cebdf13d06669baa20cc29b21cbb1
sw1ckham <sphwick@gmail.com>
2020-04-22 15:02:40 +0000

    Added Heroku and Postgres support for deployment

--- post/views.py
+++ post/views.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 from .models import Post
 from .forms import BlogPostForm
 
+
 def get_posts(request):
     """
     Create a view that will return a list
@@ -10,13 +11,13 @@ def get_posts(request):
     and render them to the blogposts.html template
     """
 
-    posts = Post.objects.filter(published_date__lte=timezone.now
-        ()).order_by('-published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, "blogposts.html", {'posts': posts})
 
+
 def post_detail(request, pk):
-    """
-    Create a view that returns a single 
+
+    """Create a view that returns a single 
     Post object based on the post ID (pk) and
     render it to the 'postdetail.html' template.
     Or return a 404 error if post is not found
@@ -25,6 +26,7 @@ def post_detail(request, pk):
     post.views += 1
     post.save()
     return render(request, 'postdetail.html', {'post': post})
+
 
 def create_or_edit_post(request, pk=None):
     """
@@ -41,4 +43,5 @@ def create_or_edit_post(request, pk=None):
             return redirect(post_detail, post.pk)
     else:
         form = BlogPostForm(instance=post)
-    return render(request, 'blogpostform.html', {'form': form})
+    return render(request, "blogpostform.html", {'form': form})
+
